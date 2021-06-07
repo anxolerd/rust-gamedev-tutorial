1cecb5adb8d2cecf98cd8886f9d4cd6520b5afdf
Oleksandr Kovalchuk <a.kovalchuk@smartweb.com.ua>
2021-06-07 18:10:39 +0300

    Rewrite code into traditional game loop
    Step 2 of the tutorial
    (https://sunjay.dev/learn-game-dev/refactor-traditional-game-loop.html)
    This removes some code duplication as well as builds the traditional
    game loop pattern for the tutorial app

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::render::WindowCanvas;
+
+fn render(canvas: &mut WindowCanvas, color: Color) {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.present();
+}
+
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("Gamedev tutorial", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
@@ -15,15 +23,10 @@ fn main() -> Result<(), String> {
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -33,9 +36,14 @@ fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+
+        // TIme management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
