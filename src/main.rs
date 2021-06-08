ffd2670665319d123c88883e73e1064cb38147c4
Oleksandr Kovalchuk <a.kovalchuk@smartweb.com.ua>
2021-06-08 20:51:32 +0300

    Render only a single sprite instead of all sprites
    https://sunjay.dev/learn-game-dev/single-sprite.html

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
-use sdl2::render::{WindowCanvas, Texture};
 // "self" imports the "image" module itself as well as everything else we listed
-use sdl2::image::{self, LoadTexture, InitFlag};
+use sdl2::image::{self, InitFlag, LoadTexture};
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(
+    canvas: &mut WindowCanvas,
+    color: Color,
+    texture: &Texture,
+    position: Point,
+    sprite: Rect,
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
-    canvas.copy(texture, None, None)?;
+    let (width, height) = canvas.output_size()?;
+
+    // Screen center as (0, 0) coordinate
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+
+    canvas.copy(texture, sprite, screen_rect)?;
     canvas.present();
 
     Ok(())
 }
-
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
@@ -23,16 +35,22 @@ fn main() -> Result<(), String> {
 
     let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("Gamedev tutorial", 800, 600)
+    let window = video_subsystem
+        .window("Gamedev tutorial", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
-    let mut canvas = window.into_canvas().build()
+    let mut canvas = window
+        .into_canvas()
+        .build()
         .expect("could not make a canvas");
 
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 26, 36);
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -40,10 +58,13 @@ fn main() -> Result<(), String> {
         // Handle events
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
-                },
+                }
                 _ => {}
             }
         }
@@ -52,7 +73,13 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+        render(
+            &mut canvas,
+            Color::RGB(i, 64, 255 - i),
+            &texture,
+            position,
+            sprite,
+        )?;
 
         // TIme management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
